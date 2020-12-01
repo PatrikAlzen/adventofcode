58106cb1f397d40fab97a76a9a99e50b7d04f4d8
PatrikAlzen <patrik.alzen@gmail.com>
2020-12-01 15:51:55 +0100

    Update day1.py

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,6 +1,18 @@
 from aocbase import AOC
+from itertools import permutations
 
-foo = AOC('./2020/1.txt')
+data = AOC('1.txt')
 
-print(foo.input_as_int)
-print(foo.input_as_string)
+combinations = permutations(data.as_int, 2)
+
+for combo in combinations:
+    if combo[0] + combo[1] == 2020:
+        print(f'Part one: {combo[0] * combo[1]}')
+        break
+
+combinations = permutations(data.as_int, 3)
+
+for combo in combinations:
+    if combo[0] + combo[1] + combo[2] == 2020:
+        print(f'Part two: {combo[0] * combo[1] * combo[2]}')
+        break
